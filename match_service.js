ccb33e324a53066a2ff963e4d7dbabde90a3c16f
Alexander <alexander.castro01@uptc.edu.co>
2024-04-12 19:28:47 -0500

    Update match_service.js

--- match_service.js
+++ match_service.js
@@ -1,3 +1,8 @@
 function match(userProfile, matchProfile){
   return userProfile.score - matchProfile.score > 20;
 }
+
+function listProfileRecommendations(userProfile){
+  ids = getRecommenedProfiles(userProfile)
+  return bd.getUserByIds(ids);
+}
