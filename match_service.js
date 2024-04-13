dcc1c8dd0e2b15938de5a59bfb94e2bb30826e35
Alexander Castro <alexander.castro01@uptc.edu.co>
2024-04-12 19:40:25 -0500

    agregue validacion para el id del perfil

--- match_service.js
+++ match_service.js
@@ -2,7 +2,10 @@ function match(userProfile, matchProfile){
   return userProfile.score - matchProfile.score > 20;
 }
 
-function listProfileRecommendations(userProfileId){
+function listProfileRecommendations(userProfileId) {
+  if (bd.get(userProfileId) == null) { 
+    return []
+  }
   ids = getRecommenedProfiles(userProfileId)
   return bd.getUserByIds(ids);
 }
